0703ef58ae486c1db289902518a8e3d89fff8c78
hkdigiverse2108 <hkdigiversework@gmail.com>
2026-03-19 17:21:58 +0530

    amount changes

--- src/controllers/dashboard/index.ts
+++ src/controllers/dashboard/index.ts
@@ -81,7 +81,8 @@ export const get_dashboard_stats = async (req, res) => {
     const paidAmount = paidAmountAgg?.[0]?.total || 0;
     const dueAmount = dueAmountAgg?.[0]?.total || 0;
     // const financialAmount = financialAmountAgg?.[0]?.total || 0;
-    const financialAmount = (incomeAmountAgg?.[0]?.total || 0) - (expenseAmountAgg?.[0]?.total || 0);
+    // const financialAmount = (incomeAmountAgg?.[0]?.total || 0) - (expenseAmountAgg?.[0]?.total || 0);
+    const financialAmount = (paidAmountAgg?.[0]?.total || 0) + (expenseAmountAgg?.[0]?.total || 0) - (incomeAmountAgg?.[0]?.total || 0);
     const incomeAmount = incomeAmountAgg?.[0]?.total || 0;
     const expenseAmount = expenseAmountAgg?.[0]?.total || 0;
 
